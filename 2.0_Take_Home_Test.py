2ad928a16f044bdaf7458fc5bdd8be9f052f7d4b
randallale <randallale@urbandaleschools.com>
2018-12-12 11:45:11 -0600

    Questions Done

--- 2.0_Take_Home_Test.py
+++ 2.0_Take_Home_Test.py
@@ -3,15 +3,15 @@ HONOR CODE: I solemnly promise that while taking this test I will only use PyCha
 but I will definitely not ask another person except the instructor. Typed: Alexander J Randall :O
 
 1.) How do you enter a single line comment in a program?
-a
+ #this is a comment
 
 
 
 2.) What do the following lines of code output? ALSO: Why do they give a different answer?
-2 / 3
-2 // 3
-3/2
-3//2
+2 / 3 == 0.6666666666666666 This is division
+2 // 3 == 0 This floors the division
+3/2 == 1.5 Again this is divison
+3//2 == 1 This floors 1.5 to 1
 
 
 
@@ -22,7 +22,7 @@ A = 22
 b = 13
 c = a+b
 
-
+This sets the variable a to equal 22 and b = to 13 when you do the equation c = a+b it really is c = 22 + 13 or 35 = 35
 
 
 
@@ -36,35 +36,35 @@ area
 area_of_rectangle
 Area_Of_Rectangle
 
-
+area_of_rectangle because it is specific and doesn't use capitals
 
 
 
 5.) Which of these variables names are NOT allowed in Python? 
 Test them if you aren't sure.
 
-apple
-Apple
-APPLE
-Apple2
-1Apple
-account number
-account_number
-account.number
-accountNumber
-account#
-pi
-PI
-fred
-Fred
-GreatBigVariable
-greatBigVariable
-great_big_variable
-great.big.variable
-2x
-x2x
-total%
-#left
+apple Allowed
+Apple Allowed
+APPLE Allowed
+Apple2 Allowed
+1Apple Not Allowed
+account number Not Allowed
+account_number Allowed
+account.number Not Allowed
+accountNumber Allowed
+account# Not Allowed
+pi Allowed
+PI Allowed
+fred Allowed
+Fred Allowed
+GreatBigVariable Allowed
+greatBigVariable Allowed
+great_big_variable Allowed
+great.big.variable Not Allowed
+2x Not Allowed
+x2x Allowed
+total% Not Allowed
+#left Not Allowed
 
 
 
@@ -72,13 +72,13 @@ total%
 
 
 6.) Predict the output of (a) and its type and then test it?
-a =2
-a*=10
-a/=2
-a+=12
-a-=7
-a
-type(a)
+a =2        == a=2
+a*=10       == a=20
+a/=2        == a=10
+a+=12       == a=22
+a-=7        == a=15
+a           == a=15
+type(a)     == float???
 
 
 
@@ -93,16 +93,16 @@ pi = x
 area = pi  * radius ** 2
 area
 
-
-
-
-
+radius=10
+pi=3.14
+area=pi*radius**2
+Area
 
 8.) What is the ouput of each of these?
-type(42)
-type(42.0)
-type("C3PO")
-type(True)
+type(42)        == <class 'int'>
+type(42.0)      == <class 'float'>
+type("C3PO")    == <class 'str'>
+type(True)      == <class 'bool'>
 
 
 
@@ -112,7 +112,7 @@ type(True)
 
 9.) Fix the mistake in the following code:
 x = 4
-y = 5
+y = 5       the y was a capital
 a = 3(x + y)
 a
 
@@ -126,8 +126,9 @@ x,y,z =(3,4,5)
 ave = x+y+z/3
 ave
 
-
-
-
-
+Its dividing the z not the sum
+To fix it you should use a parenthesis
+x,y,z =(3,4,5)
+ave = (x+y+z)/3
+ave
 '''
