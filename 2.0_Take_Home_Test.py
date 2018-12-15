b20e18fdc25eb57459cfa8f8adf7cdb49b5dc2f5
Randallale <randallale@urbandaleschools.com>
2018-12-15 16:12:41 -0600

    FIXED it

--- 2.0_Take_Home_Test.py
+++ 2.0_Take_Home_Test.py
@@ -16,7 +16,7 @@ but I will definitely not ask another person except the instructor. Typed: Alexa
 
 
 
-INCORRECT!
+INCORRECT! - CHECKED
 3.) What happens when you try this and why?
 A = 22
 b = 13
@@ -109,7 +109,7 @@ type(True)      == <class 'bool'>
 
 
 
-INCORRECT!
+INCORRECT! - CHECKED
 9.) Fix the mistake in the following code:
 x = 4
 y = 5       the y was a capital
